789da78c8328a6ef54b4177ad14dbe70864d1b39
Darrien Lam <lamdarr@sheridancollege.ca>
2025-01-29 14:15:41 -0500

    Fixed asset::reset() error

--- GameEngineFramework/Resource.cpp
+++ GameEngineFramework/Resource.cpp
@@ -37,3 +37,5 @@ void Resource::DeserializeAsset(std::istream& _stream, Asset*& _asset) {
 void Resource::Serialize(std::ostream& _stream) {}
 
 void Resource::Deserialize(std::istream& _stream) {}
+
+void Resource::Reset() {}
