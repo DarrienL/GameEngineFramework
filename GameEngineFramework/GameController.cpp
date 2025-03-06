1cafcf12c14ccb9dcfa8421cb6d5621cef75e706
Darrien Lam <lamdarr@sheridancollege.ca>
2025-03-06 11:47:23 -0500

    Scaling

--- GameEngineFramework/GameController.cpp
+++ GameEngineFramework/GameController.cpp
@@ -30,8 +30,12 @@ void GameController::RunGame() {
     sheet->AddAnimation(EN_AN_IDLE, 0, 6, 6.0f);
     sheet->AddAnimation(EN_AN_RUN, 6, 8, 6.0f);
 
+    RenderTarget* rt = new RenderTarget();
+    rt->Create(NATIVE_XRES, NATIVE_YRES);
+
     while (m_sdlEvent.type != SDL_QUIT) {
         t->Tick();
+        rt->Start();
         r->SetDrawColor(Color(255, 255, 255, 255));
         r->ClearScreen();
         r->RenderTexture(sheet, sheet->Update(EN_AN_RUN, t->GetDeltaTime()), Rect(0, 150, 69 * 3, 150 + 44 * 3));
@@ -42,10 +46,15 @@ void GameController::RunGame() {
         std::string s = "Frame number: " + std::to_string(sheet->GetCurrentClip(EN_AN_RUN));
         font20->Write(r->GetRenderer(), s.c_str(), SDL_Color{ 0, 255, 0 }, SDL_Point{ 250, 200 });
 
+        rt->Stop();
+        r->SetDrawColor(Color(0, 0, 0, 255));
+        r->ClearScreen();
+        rt->Render(t->GetDeltaTime());
         SDL_RenderPresent(r->GetRenderer());
         t->CapFPS();
     }
 
+    delete rt;
     delete SpriteAnim::Pool;
     delete SpriteSheet::Pool;
     font20->Shutdown();
