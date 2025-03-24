87bb54f648e767b651ae7d33c458a27b785cb597
Darrien Lam <lamdarr@sheridancollege.ca>
2025-03-24 16:29:01 -0400

    Multiply loop

--- GameEngineFramework/main.cpp
+++ GameEngineFramework/main.cpp
@@ -1,18 +1,17 @@
 #include "StandardIncludes.h"
 
-static void AddVectors() {
-	glm::vec2 a = { 3, 5 };
-	glm::vec2 b = { 2, -1 };
-	glm::vec2 c = a + b;
-	std::cout << glm::to_string(c) << std::endl;
-
-	glm::vec3 d = { 3, 5, 1 };
-	glm::vec3 e = { 2, -1, -1 };
-	glm::vec3 f = d + e;
-	std::cout << glm::to_string(f) << std::endl;
+static glm::vec3 pos = { 2, 2, 2 };
+static void MultiplyVectors() {
+	glm::vec3 dest = { 1, 0, 0 };
+	glm::vec3 dir = dest - pos;
+	dir = glm::normalize(dir);
+	pos += (dir * 0.1f);
+	std::cout << glm::to_string(pos) << std::endl;
 }
 
 int main() {
-	AddVectors();
+	for (int count = 0; count < 10; count++) {
+		MultiplyVectors();
+	}
 	return 0;
 }
