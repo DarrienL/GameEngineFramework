e98e2cbeb810f9769ea17ea860c139089c354490
Darrien Lam <lamdarr@sheridancollege.ca>
2025-03-25 19:45:10 -0400

    Initial commit

--- GameEngineFramework/main.cpp
+++ GameEngineFramework/main.cpp
@@ -1,30 +1,14 @@
 #include "StandardIncludes.h"
 
-static void DotProduct() {
-	glm::vec3 source = { 0, 0, 0 };
-	glm::vec3 a = { 0, 2, 0 };
-	glm::vec3 b = { 1, 1, 0 };
-	glm::vec3 aVec = a - source;
-	glm::vec3 bVec = b - source;
-	aVec = glm::normalize(aVec);
-	bVec = glm::normalize(bVec);
-	float dot = glm::dot(aVec, bVec);
-	std::cout << "Dot = " << dot << std::endl;
+vector<glm::vec3> Task3() {
+
 }
 
-static void CrossProduct() {
-	glm::vec3 source = { 0, 0, 0 };
-	glm::vec3 a = { 0, 2, 0 };
-	glm::vec3 b = { 1, 1, 0 };
-	glm::vec3 aVec = a - source;
-	glm::vec3 bVec = b - source;
-	glm::vec3 cross = glm::cross(aVec, bVec);
-	cross = glm::normalize(cross);
-	std::cout << "Cross = " << glm::to_string(cross) << std::endl;
-}
 
 int main() {
-	DotProduct();
-	CrossProduct();
+	// Ask the user for 3 points of a triangle which forms half of one side of a cuboid
+	// Construct a 3D cuboid using the 3 supplied triangle points. You can choose any depth for the cube.
+	
+
 	return 0;
 }
