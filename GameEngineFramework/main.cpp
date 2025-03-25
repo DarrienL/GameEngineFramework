b324c7774446fdc3ed93a793eb7bf05fb5861695
Darrien Lam <lamdarr@sheridancollege.ca>
2025-03-25 12:54:02 -0400

    Task 2

--- GameEngineFramework/main.cpp
+++ GameEngineFramework/main.cpp
@@ -1,21 +1,27 @@
 #include "StandardIncludes.h"
 
-int task1(glm::vec3 pos, glm::vec3 dest, float speed) {
-	glm::vec3 dir = dest - pos;
-	dir = glm::normalize(dir);
-	int steps = 0;
-	while (glm::distance(pos, dest) > speed) {
-		pos += (dir * speed);
-		steps++;
-	}
-	return steps;
+float task2Perimeter(glm::vec3 a, glm::vec3 b, glm::vec3 c) {
+	// Calculate perimeter
+	float ab = glm::distance(a, b);
+	float bc = glm::distance(b, c);
+	float ca = glm::distance(c, a);
+	return ab + bc + ca; // Perimeter = sum of all 3 sides (distances between points)
+}
+
+float task2Area(glm::vec3 a, glm::vec3 b, glm::vec3 c) {
+	// Calculate area
+	glm::vec3 ab = b - a;
+	glm::vec3 ac = c - a;
+	glm::vec3 cross = glm::cross(ab, ac);
+	return 0.5f * glm::length(cross); // Area = 0.5 * magnitude of cross product of 2 sides
 }
 
 int main() {
-	glm::vec3 a = { 3, 0, 8 };
-	glm::vec3 b = { 9, 2, 1 };
-	float s = 0.2f;
-	cout << "It would take " << to_string(task1(a, b, s)) << " steps to reach the destination." << endl;
-
+	// Input
+	glm::vec3 a = { 0, 0, 0 };
+	glm::vec3 b = { 2, 0, 0 };
+	glm::vec3 c = { 0, 2, 0 };
+	cout << "The perimeter of the traingle is " << to_string(task2Perimeter(a, b, c)) << endl;
+	cout << "The area of the traingle is " << to_string(task2Area(a, b, c)) << endl;
 	return 0;
 }
