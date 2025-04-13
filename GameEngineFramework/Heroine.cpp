68506cbdfbd25916352e96be8e5eda1ad2d5cb23
Darrien Lam <lamdarr@sheridancollege.ca>
2025-04-13 00:03:19 -0400

    Fixed typo

--- GameEngineFramework/Heroine.cpp
+++ GameEngineFramework/Heroine.cpp
@@ -9,7 +9,7 @@ Heroine::Heroine() {
 	m_rigidBody = PhysicsController::Instance().AddRigidBody(glm::vec2{ 100, 200 }, glm::vec2(0, 0), 40);
 	m_animations = SpriteSheet::Pool->GetResource();
 	m_animations->Load("../Assets/Textures/Warrior.tga");
-	m_animations->SetSize(17, 0, 69, 44);
+	m_animations->SetSize(17, 6, 69, 44);
 	m_animations->AddAnimation(EN_AN_IDLE, 0, 6, 10.0f);
 	m_animations->AddAnimation(EN_AN_JUMP_UP_FALL, 41, 8, 4.0f);
 	m_animations->AddAnimation(EN_AN_CROUCH, 64, 4, 8.0f);
